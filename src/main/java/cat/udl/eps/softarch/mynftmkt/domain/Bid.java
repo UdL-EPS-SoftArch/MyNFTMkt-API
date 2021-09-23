2a70455794a0aa42d5295e07e4ac8b9f1ca5a8d3
Tibieriu <tibipaiu97@gmail.com>
2021-09-23 16:34:04 +0200

    Created Bid Model

---
+++ src/main/java/cat/udl/eps/softarch/mynftmkt/domain/Bid.java
@@ -0,0 +1,23 @@
+package cat.udl.eps.softarch.mynftmkt.domain;
+
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class Bid extends UriEntity<Long> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private ZonedDateTime dataTime;
+
+    private BigDecimal price;
+}
