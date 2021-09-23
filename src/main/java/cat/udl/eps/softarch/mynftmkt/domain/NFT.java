971ad525938bcd0504d48a9d51e7bd7a61cc5bd3
MarioMarti <martisiso99@gmail.com>
2021-09-23 16:46:01 +0200

    First Commit. NFT class created without getters and setters

--- src/main/java/cat/udl/eps/softarch/mynftmkt/domain/NFT.java
+++ src/main/java/cat/udl/eps/softarch/mynftmkt/domain/NFT.java
@@ -4,10 +4,7 @@ package cat.udl.eps.softarch.mynftmkt.domain;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import javax.persistence.ElementCollection;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.List;
 
 @Entity
@@ -16,6 +13,9 @@ import java.util.List;
 public class NFT extends UriEntity<String>{
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private String title;
 
     private String description;
@@ -23,55 +23,12 @@ public class NFT extends UriEntity<String>{
     @ElementCollection
     private List<String> keywords;
 
-    public String getCategory() {
-        return category;
-    }
-
     private String category;
 
     private String mediaType;
 
     private String content;
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 
 
-    public String getMediaType() {
-        return mediaType;
-    }
-
-    public void setMediaType(String mediaType) {
-        this.mediaType = mediaType;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    @Override
-    public String getId() {
-        return getTitle();
-    }
 }
