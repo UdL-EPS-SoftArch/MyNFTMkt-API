5200349310c142d84dbcf695e930fcde30ead149
Emina33 <emi.handzic@gmail.com>
2021-10-05 17:15:30 +0200

    Delete user (Cucumber)

--- src/test/java/cat/udl/eps/softarch/mynftmkt/steps/DeleteUserStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/mynftmkt/steps/DeleteUserStepDefs.java
@@ -1,2 +1,44 @@
-package cat.udl.eps.softarch.mynftmkt.steps;public class DeleteUserStepDefs {
+package cat.udl.eps.softarch.mynftmkt.steps;
+
+import cat.udl.eps.softarch.mynftmkt.domain.User;
+import cat.udl.eps.softarch.mynftmkt.repository.UserRepository;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+public class DeleteUserStepDefs {
+    final StepDefs stepDefs;
+    final UserRepository userRepository;
+
+    DeleteUserStepDefs(StepDefs stepDefs, UserRepository userRepository) {
+        this.stepDefs = stepDefs;
+        this.userRepository = userRepository;
+    }
+
+   /* @Given("There is a registered user with username {string} and password {string} and email {string} and name {string} and lastname {string}")
+    public void thereIsARegisteredUserWithUsernameAndPasswordAndEmailAndNameAndLastname(String username, String password, String email, String name, String lastname) {
+        if (!userRepository.existsById(username)) {
+            User user = new User();
+            user.setEmail(email);
+            user.setUsername(username);
+            user.setPassword(password);
+            user.setName(name);
+            user.setLastname(lastname);
+            user.encodePassword();
+            userRepository.save(user);
+        }
+    }
+
+    @When("I delete the user {string} with password {string}")
+    public void iDeleteTheUserWith(String username, String password1) {
+        if (userRepository.existsById(username)) {
+            userRepository.deleteById(username);
+        }
+    }
+
+
+*/
 }
---
+++ src/test/resources/features/DeleteUser.feature
@@ -0,0 +1,21 @@
+Feature: Delete User
+  In order to control who uses the app
+  As an administrator
+  I want to delete authorised users
+
+  Background:
+    Given There is a registered administrator with username "admin" and password "password" and email "admin@sample.app"
+
+  Scenario: Delete an existing user
+    Given There is a registered user with username "user"
+    When I delete a user with username "user", email "user@sample.app" and password "password"
+    Then The response code is 201
+    And I cannot login with username "user" and password "password"
+
+  Scenario: Delete user with non-existing username
+    Given I login as "admin" with password "password"
+    When I delete a user with username "newuser", email "newuser@sample.app" and password "newpassword"
+    Then The response code is 404
+    And The error message is "user does not exist"
+
+
