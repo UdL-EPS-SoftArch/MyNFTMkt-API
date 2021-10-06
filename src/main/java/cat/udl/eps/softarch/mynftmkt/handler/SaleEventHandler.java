1343659da4b820b532f5be0275338d6798e1cab4
sergisabate <sergisabatevila@gmail.com>
2021-10-06 16:41:35 +0200

    Fixed SaleEventHandler

--- src/main/java/cat/udl/eps/softarch/mynftmkt/handler/SaleEventHandler.java
+++ src/main/java/cat/udl/eps/softarch/mynftmkt/handler/SaleEventHandler.java
@@ -2,9 +2,13 @@ package cat.udl.eps.softarch.mynftmkt.handler;
 
 import cat.udl.eps.softarch.mynftmkt.domain.Sale;
 import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.stereotype.Component;
 
 import java.time.ZonedDateTime;
 
+@Component
+@RepositoryEventHandler
 public class SaleEventHandler {
 
     @HandleBeforeCreate
