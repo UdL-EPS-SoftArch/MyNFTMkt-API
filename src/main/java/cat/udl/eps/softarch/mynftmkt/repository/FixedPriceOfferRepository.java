23dd6737e075377c2d3c8754f7e169ae1c48405b
rogerLarriba <roglarbau@gmail.com>
2021-10-26 17:55:04 +0200

    findByPrice in FixedPriceOfferRepository

--- src/main/java/cat/udl/eps/softarch/mynftmkt/repository/FixedPriceOfferRepository.java
+++ src/main/java/cat/udl/eps/softarch/mynftmkt/repository/FixedPriceOfferRepository.java
@@ -2,11 +2,17 @@ package cat.udl.eps.softarch.mynftmkt.repository;
 
 import cat.udl.eps.softarch.mynftmkt.domain.FixedPriceOffer;
 import cat.udl.eps.softarch.mynftmkt.domain.NFT;
+import cat.udl.eps.softarch.mynftmkt.domain.Offer;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 
 @RepositoryRestResource
 public interface FixedPriceOfferRepository extends PagingAndSortingRepository<FixedPriceOffer, Long> {
+    public List<FixedPriceOffer> findByPriceIsLessThanEqual(@Param("price") BigDecimal price);
+
 }
