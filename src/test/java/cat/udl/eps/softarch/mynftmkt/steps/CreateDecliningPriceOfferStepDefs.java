14e1b953bf32a4717fef77af39787d096b7b16b1
sergisabate <sergisabatevila@gmail.com>
2021-10-28 16:35:44 +0200

    CreateDecliningPriceOffer 2 Fix

--- src/test/java/cat/udl/eps/softarch/mynftmkt/steps/CreateDecliningPriceOfferStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/mynftmkt/steps/CreateDecliningPriceOfferStepDefs.java
@@ -1,8 +1,19 @@
 package cat.udl.eps.softarch.mynftmkt.steps;
 
+import cat.udl.eps.softarch.mynftmkt.repository.NFTRepository;
 import io.cucumber.java.en.When;
 
-public class CreateDecliningPriceOffer {
+public class CreateDecliningPriceOfferStepDefs {
+
+    final StepDefs stepDefs;
+    final NFTRepository nftRepository;
+
+    public CreateDecliningPriceOfferStepDefs(StepDefs stepDefs, NFTRepository nftRepository) {
+        this.stepDefs = stepDefs;
+        this.nftRepository = nftRepository;
+    }
+
+
     @When("I create a Declining Price Offer with an starting price at {string}, an ending price at {string} and an expiration to {string} days with the previous NFT.")
     public void iCreateADecliningPriceOfferWithAnStartingPriceAtAnEndingPriceAtAndAnExpirationToDaysWithThePreviousNFT(String starting, String ending, String expiration) {
     }
