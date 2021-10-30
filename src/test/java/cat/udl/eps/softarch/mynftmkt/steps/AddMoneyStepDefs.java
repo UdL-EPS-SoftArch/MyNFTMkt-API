8fc320dc45bb577f8bc3334051401751178eecc4
Sanel Novalić <sanel.novalic@edu.fit.ba>
2021-10-30 18:30:31 +0200

    Implemented function 'Given a user has a balance of 0'

--- src/test/java/cat/udl/eps/softarch/mynftmkt/steps/AddMoneyStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/mynftmkt/steps/AddMoneyStepDefs.java
@@ -1,10 +1,12 @@
 package cat.udl.eps.softarch.mynftmkt.steps;
 
+import cat.udl.eps.softarch.mynftmkt.domain.Bid;
 import cat.udl.eps.softarch.mynftmkt.domain.User;
 import cat.udl.eps.softarch.mynftmkt.repository.UserRepository;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
 import org.json.JSONObject;
+import org.junit.Assert;
 import org.springframework.http.MediaType;
 
 import java.math.BigDecimal;
@@ -14,7 +16,8 @@ import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.hamcrest.Matchers;
 
 public class AddMoneyStepDefs {
     final StepDefs stepDefs;
@@ -27,16 +30,30 @@ public class AddMoneyStepDefs {
     @When("I add {string} money to the user {string}")
     public void iAddMoneyToUser(String balance, String username) throws Exception {
         Optional<User> user = userRepository.findById(username);
+
+        // Converting the string into BigDecimal
         BigDecimal bigDecimal = new BigDecimal(balance);
-        JSONObject newBalance = new JSONObject();
-        newBalance.put("balance",user.get().getBalance().add(bigDecimal));
+
+        // Adding money to the user
+        BigDecimal addedBalance = user.orElse(new User()).getBalance().add(bigDecimal); // orElse is a non-exception get() method
+
+        // JSONObject to patch the balance
+        JSONObject balanceToBeAdded = new JSONObject();
+        balanceToBeAdded.put("balance", addedBalance);
 
         stepDefs.result = stepDefs.mockMvc.perform(
                         patch("/users/{username}", username)
                                 .contentType(MediaType.APPLICATION_JSON)
-                                .content(newBalance.toString())
+                                .content(balanceToBeAdded.toString())
                                 .accept(MediaType.APPLICATION_JSON)
                                 .with(AuthenticationStepDefs.authenticate()))
                 .andDo(print());
+    }
+    @And("Given a user {string} has a balance of {string}")
+    public void givenAUserHasABalanceOf(String username, String balance) {
+        User user = userRepository.findById(username).orElse(new User()); // orElse is a non-exception get() method
+
+        Assert.assertEquals("user \""
+                + username + "\"should have a balance of 0", 0, user.getBalance().compareTo(BigDecimal.ZERO));
     }
 }
