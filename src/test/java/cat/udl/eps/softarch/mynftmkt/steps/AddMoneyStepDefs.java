d5618f12e257c3e6310a83aaf3847452d6eee18f
Sanel Novalić <sanel.novalic@edu.fit.ba>
2021-10-30 18:37:26 +0200

    Implemented function to check if the balance was added or not

--- src/test/java/cat/udl/eps/softarch/mynftmkt/steps/AddMoneyStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/mynftmkt/steps/AddMoneyStepDefs.java
@@ -1,6 +1,5 @@
 package cat.udl.eps.softarch.mynftmkt.steps;
 
-import cat.udl.eps.softarch.mynftmkt.domain.Bid;
 import cat.udl.eps.softarch.mynftmkt.domain.User;
 import cat.udl.eps.softarch.mynftmkt.repository.UserRepository;
 import io.cucumber.java.en.And;
@@ -16,8 +15,6 @@ import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.hamcrest.MatcherAssert.assertThat;
-import org.hamcrest.Matchers;
 
 public class AddMoneyStepDefs {
     final StepDefs stepDefs;
@@ -55,5 +52,19 @@ public class AddMoneyStepDefs {
 
         Assert.assertEquals("user \""
                 + username + "\"should have a balance of 0", 0, user.getBalance().compareTo(BigDecimal.ZERO));
+    }
+    @And("It has been added a balance {string} to the user {string}")
+    public void itHasBeenAddedABalanceToTheUser(String bigDecimal, String username) throws Exception {
+        // Converting the string into BigDecimal
+        BigDecimal balance = new BigDecimal(bigDecimal);
+        Optional<User> user = userRepository.findById(username);
+
+        stepDefs.result = stepDefs.mockMvc.perform(
+                        get("/users/{username}", username)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .with(AuthenticationStepDefs.authenticate()))
+                .andDo(print())
+                .andExpect(jsonPath("$.balance", is(balance),BigDecimal.class));
+        // jsonPath converts to a double, configuration needs to be changed in order for BigDecimal to work for precisions greater than 2
     }
 }
