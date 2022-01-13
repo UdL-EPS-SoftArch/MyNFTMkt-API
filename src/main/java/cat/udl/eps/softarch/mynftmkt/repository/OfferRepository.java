e4ba09248fd57378110cbc41a4105efbdec12cc0
Sanel Novalić <sanel.novalic@edu.fit.ba>
2022-01-13 14:22:53 +0100

    findByNftOrderByDateTime

--- src/main/java/cat/udl/eps/softarch/mynftmkt/repository/OfferRepository.java
+++ src/main/java/cat/udl/eps/softarch/mynftmkt/repository/OfferRepository.java
@@ -6,7 +6,10 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface OfferRepository extends PagingAndSortingRepository<Offer, Long> {
     public boolean existsByNft(@Param("nft") NFT nft);
+    public List<Offer> findByNftOrderByDateTime(@Param("nft") NFT nft);
 }
