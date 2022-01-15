6122d369c373e6df1371f0e7e402648c34b54fbf
Arnau Tallaví <atallavi@gmail.com>
2022-01-15 20:17:48 +0100

    Handlers for when updating and deleting NFT's

--- src/main/java/cat/udl/eps/softarch/mynftmkt/handler/NFTEventHandler.java
+++ src/main/java/cat/udl/eps/softarch/mynftmkt/handler/NFTEventHandler.java
@@ -2,12 +2,18 @@ package cat.udl.eps.softarch.mynftmkt.handler;
 
 import cat.udl.eps.softarch.mynftmkt.domain.NFT;
 import cat.udl.eps.softarch.mynftmkt.domain.User;
+import cat.udl.eps.softarch.mynftmkt.exception.ForbiddenException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
+import org.springframework.data.rest.core.annotation.HandleBeforeDelete;
+import org.springframework.data.rest.core.annotation.HandleBeforeSave;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+
+import java.util.Collection;
 
 @Component
 @RepositoryEventHandler
@@ -21,5 +27,31 @@ public class NFTEventHandler {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         nft.setAuthor(user);
         nft.setOwner(user);
+    }
+
+    @HandleBeforeDelete
+    public void handleNFTTBeforeDelete(NFT nft){
+        logger.info("Before deleting NFT{}", nft.toString());
+        if (!userIsOwner(nft) & !userIsAdmin()){
+            throw new ForbiddenException();
+        }
+    }
+
+    @HandleBeforeSave
+    public void handleNFTTBeforeSave(NFT nft){
+        logger.info("Before editing NFT{}", nft.toString());
+        if (!userIsOwner(nft) & !userIsAdmin()){
+            throw new ForbiddenException();
+        }
+    }
+
+    private boolean userIsAdmin(){
+        Collection<? extends GrantedAuthority> userAuthority = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+        return userAuthority.stream().anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ROLE_ADMIN"));
+    }
+
+    private boolean userIsOwner(NFT nft){
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return user.getId().equals(nft.getOwner());
     }
 }
